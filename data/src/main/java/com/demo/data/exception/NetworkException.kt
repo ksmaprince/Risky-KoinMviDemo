08663fff4d8a0a588c1ab3108fdcafdc40763ed7
KHUN SOE MOE AUNG <khunsoemoe.aung@kbzbank.com>
2022-06-16 10:46:00 +0630

    FirstTimeCommit

---
+++ data/src/main/java/com/demo/data/exception/NetworkException.kt
@@ -0,0 +1,22 @@
+package com.demo.data.exception
+
+import java.lang.Exception
+
+sealed class NetworkException: Exception(){
+
+    class ApiException(override val message: String): NetworkException()
+
+    object TimeOutException : NetworkException()
+
+    class DefaultException(override val message: String) : NetworkException()
+
+    class NoConnectivityException : NetworkException() {
+        override val message: String?
+            get() = "No network available, please check your WiFi or Data connection"
+    }
+
+    class NoInternetException : NetworkException() {
+        override val message: String?
+            get() = "No internet available, please check your connected WIFi or Data"
+    }
+}
